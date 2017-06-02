3db5d918b5ccec49376c924095c820e6a98d2cfd
Michael Adaixo <michael.adaixo@gmail.com>
2017-06-02 11:38:11 +0100

    Singleton pattern

---
+++ Assets/DesignPatterns/Singleton/FileSystem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// .
+/// http://gameprogrammingpatterns.com/singleton.html
+/// </summary>
+
+public class FileSystem
+{
+    public static FileSystem Instance()
+    {
+#if PLATFORM == PLAYSTATION3
+        FileSystem instance = new PS3FileSystem();
+#elif PLATFORM == WII
+        FileSystem instance = new WiiFileSystem();
+#endif
+        return instance;
+    }
+    
+	
+    protected FileSystem( )
+    {
+
+    }
+
+    public virtual string ReadFile(string path)
+    {
+        return "";
+    }
+    public virtual void WriteFile(string path, string contents)
+    {
+
+    }
+}
+
+
+public class PS3FileSystem : FileSystem
+{
+
+    public override string ReadFile(string path)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void WriteFile(string path, string contents)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class WiiFileSystem : FileSystem
+{
+
+    public override string ReadFile(string path)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void WriteFile(string path, string contents)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ Assets/DesignPatterns/Singleton/SingletonTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonTest : MonoBehaviour
+{
+    private void Start()
+    {
+        FileSystem ps3Fs = FileSystem.Instance();
+        Debug.Log(ps3Fs.GetType());
+
+    }
+}
